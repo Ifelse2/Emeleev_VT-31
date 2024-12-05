983d8f14a6d25dc2be8a40f2f3c8f9c40f8666e4
Emeleev_VT-31 <75689751+Ifelse2@users.noreply.github.com>
2024-12-05 09:15:42 +0400

    Add files via upload

---
+++ LAB_1/LAB_1/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB_1
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<char, string> dictWindows1251 = new Dictionary<char, string>
+{
+       {'А', "0xC0"}, {'Б', "0xC1"}, {'В', "0xC2"}, {'Г', "0xC3"}, {'Д', "0xC4"}, {'Е', "0xC5"}, {'Ж', "0xC6"}, {'З', "0xC7"},
+    {'И', "0xC8"}, {'Й', "0xC9"}, {'К', "0xCA"}, {'Л', "0xCB"}, {'М', "0xCC"}, {'Н', "0xCD"}, {'О', "0xCE"}, {'П', "0xCF"},
+    {'Р', "0xD0"}, {'С', "0xD1"}, {'Т', "0xD2"}, {'У', "0xD3"}, {'Ф', "0xD4"}, {'Х', "0xD5"}, {'Ц', "0xD6"}, {'Ч', "0xD7"},
+    {'Ш', "0xD8"}, {'Щ', "0xD9"}, {'Ъ', "0xDA"}, {'Ы', "0xDB"}, {'Ь', "0xDC"}, {'Э', "0xDD"}, {'Ю', "0xDE"}, {'Я', "0xDF"},
+    {'а', "0xE0"}, {'б', "0xE1"}, {'в', "0xE2"}, {'г', "0xE3"}, {'д', "0xE4"}, {'е', "0xE5"}, {'ж', "0xE6"}, {'з', "0xE7"},
+    {'и', "0xE8"}, {'й', "0xE9"}, {'к', "0xEA"}, {'л', "0xEB"}, {'м', "0xEC"}, {'н', "0xED"}, {'о', "0xEE"}, {'п', "0xEF"},
+    {'р', "0xF0"}, {'с', "0xF1"}, {'т', "0xF2"}, {'у', "0xF3"}, {'ф', "0xF4"}, {'х', "0xF5"}, {'ц', "0xF6"}, {'ч', "0xF7"},
+    {'ш', "0xF8"}, {'щ', "0xF9"}, {'ъ', "0xFA"}, {'ы', "0xFB"}, {'ь', "0xFC"}, {'э', "0xFD"}, {'ю', "0xFE"}, {'я', "0xFF"},
+    {'Ё', "0xA8"}, {'ё', "0xB8"},
+    {'A', "0x41"}, {'B', "0x42"}, {'C', "0x43"}, {'D', "0x44"}, {'E', "0x45"}, {'F', "0x46"}, {'G', "0x47"}, {'H', "0x48"},
+    {'I', "0x49"}, {'J', "0x4A"}, {'K', "0x4B"}, {'L', "0x4C"}, {'M', "0x4D"}, {'N', "0x4E"}, {'O', "0x4F"}, {'P', "0x50"},
+    {'Q', "0x51"}, {'R', "0x52"}, {'S', "0x53"}, {'T', "0x54"}, {'U', "0x55"}, {'V', "0x56"}, {'W', "0x57"}, {'X', "0x58"},
+    {'Y', "0x59"}, {'Z', "0x5A"}, {'a', "0x61"}, {'b', "0x62"}, {'c', "0x63"}, {'d', "0x64"}, {'e', "0x65"}, {'f', "0x66"},
+    {'g', "0x67"}, {'h', "0x68"}, {'i', "0x69"}, {'j', "0x6A"}, {'k', "0x6B"}, {'l', "0x6C"}, {'m', "0x6D"}, {'n', "0x6E"},
+    {'o', "0x6F"}, {'p', "0x70"}, {'q', "0x71"}, {'r', "0x72"}, {'s', "0x73"}, {'t', "0x74"}, {'u', "0x75"}, {'v', "0x76"},
+    {'w', "0x77"}, {'x', "0x78"}, {'y', "0x79"}, {'z', "0x7A"}, {'0', "0x30"}, {'1', "0x31"}, {'2', "0x32"}, {'3', "0x33"},
+    {'4', "0x34"}, {'5', "0x35"}, {'6', "0x36"}, {'7', "0x37"}, {'8', "0x38"}, {'9', "0x39"},
+    {' ', "0x20"}, {'.', "0x2E"}, {',', "0x2C"}, {'!', "0x21"}, {'?', "0x3F"}
+};
+Dictionary<string, char> dictWindows1251Reversed = new Dictionary<string, char>
+{
+    {"0xC0", 'А'}, {"0xC1", 'Б'}, {"0xC2", 'В'}, {"0xC3", 'Г'}, {"0xC4", 'Д'}, {"0xC5", 'Е'}, {"0xC6", 'Ж'}, {"0xC7", 'З'},
+    {"0xC8", 'И'}, {"0xC9", 'Й'}, {"0xCA", 'К'}, {"0xCB", 'Л'}, {"0xCC", 'М'}, {"0xCD", 'Н'}, {"0xCE", 'О'}, {"0xCF", 'П'},
+    {"0xD0", 'Р'}, {"0xD1", 'С'}, {"0xD2", 'Т'}, {"0xD3", 'У'}, {"0xD4", 'Ф'}, {"0xD5", 'Х'}, {"0xD6", 'Ц'}, {"0xD7", 'Ч'},
+    {"0xD8", 'Ш'}, {"0xD9", 'Щ'}, {"0xDA", 'Ъ'}, {"0xDB", 'Ы'}, {"0xDC", 'Ь'}, {"0xDD", 'Э'}, {"0xDE", 'Ю'}, {"0xDF", 'Я'},
+    {"0xE0", 'а'}, {"0xE1", 'б'}, {"0xE2", 'в'}, {"0xE3", 'г'}, {"0xE4", 'д'}, {"0xE5", 'е'}, {"0xE6", 'ж'}, {"0xE7", 'з'},
+    {"0xE8", 'и'}, {"0xE9", 'й'}, {"0xEA", 'к'}, {"0xEB", 'л'}, {"0xEC", 'м'}, {"0xED", 'н'}, {"0xEE", 'о'}, {"0xEF", 'п'},
+    {"0xF0", 'р'}, {"0xF1", 'с'}, {"0xF2", 'т'}, {"0xF3", 'у'}, {"0xF4", 'ф'}, {"0xF5", 'х'}, {"0xF6", 'ц'}, {"0xF7", 'ч'},
+    {"0xF8", 'ш'}, {"0xF9", 'щ'}, {"0xFA", 'ъ'}, {"0xFB", 'ы'}, {"0xFC", 'ь'}, {"0xFD", 'э'}, {"0xFE", 'ю'}, {"0xFF", 'я'},
+    {"0xA8", 'Ё'}, {"0xB8", 'ё'},
+    {"0x41", 'A'}, {"0x42", 'B'}, {"0x43", 'C'}, {"0x44", 'D'}, {"0x45", 'E'}, {"0x46", 'F'}, {"0x47", 'G'}, {"0x48", 'H'},
+    {"0x49", 'I'}, {"0x4A", 'J'}, {"0x4B", 'K'}, {"0x4C", 'L'}, {"0x4D", 'M'}, {"0x4E", 'N'}, {"0x4F", 'O'}, {"0x50", 'P'},
+    {"0x51", 'Q'}, {"0x52", 'R'}, {"0x53", 'S'}, {"0x54", 'T'}, {"0x55", 'U'}, {"0x56", 'V'}, {"0x57", 'W'}, {"0x58", 'X'},
+    {"0x59", 'Y'}, {"0x5A", 'Z'}, {"0x61", 'a'}, {"0x62", 'b'}, {"0x63", 'c'}, {"0x64", 'd'}, {"0x65", 'e'}, {"0x66", 'f'},
+    {"0x67", 'g'}, {"0x68", 'h'}, {"0x69", 'i'}, {"0x6A", 'j'}, {"0x6B", 'k'}, {"0x6C", 'l'}, {"0x6D", 'm'}, {"0x6E", 'n'},
+    {"0x6F", 'o'}, {"0x70", 'p'}, {"0x71", 'q'}, {"0x72", 'r'}, {"0x73", 's'}, {"0x74", 't'}, {"0x75", 'u'}, {"0x76", 'v'},
+    {"0x77", 'w'}, {"0x78", 'x'}, {"0x79", 'y'}, {"0x7A", 'z'}, {"0x30", '0'}, {"0x31", '1'}, {"0x32", '2'}, {"0x33", '3'},
+    {"0x34", '4'}, {"0x35", '5'}, {"0x36", '6'}, {"0x37", '7'}, {"0x38", '8'}, {"0x39", '9'},
+    {"0x20", ' '}, {"0x2E", '.'}, {"0x2C", ','}, {"0x21", '!'}, {"0x3F", '?'}
+};
+        public string ded(string text)
+        {
+            string res = "";
+            int n = text.Length;
+        for(int i = 0; i < n; i++)
+            {
+                if (dictWindows1251.ContainsKey(text[i])){
+                    res += dictWindows1251[text[i]];
+                }
+            }
+            return res;
+        }
+        public string life(string text)
+        {
+            string a = "";
+            string res = "";
+            int n = text.Length;
+            for (int i = 0; i < n; i++)
+            {    
+                if (i % 4 == 0)
+                {
+                    a += " ";
+                }
+                a += text[i];
+            }
+
+            string[] test = a.Split(' ');
+            
+            foreach (string i in test)
+            {
+                if (dictWindows1251Reversed.ContainsKey(i))
+                {
+                    res += dictWindows1251Reversed[i];
+                }
+            }
+            return res;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+
+            StringBuilder binaryString = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                binaryString.Append(Convert.ToString(c, 2).PadLeft(8, '0') + " ");
+            }
+            textBox2.Text = binaryString.ToString().Trim();
+            textBox3.Text = ded(input);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string binaryInput = textBox3.Text;
+            string b = life(binaryInput);
+            textBox1.Text += (b);
+        }
+    }
+}
